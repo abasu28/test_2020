09e3209173690d247b33f39c95d10a7ba499a852
AB test <ab_test@localhost.localdomain>
2020-04-24 12:12:06 -0400

    v1.0

---
+++ test_script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#
+
+echo "\n***********************"
+echo "This is to validate a sample script - trigegred from Host to a Ubuntu docker image ::"
+whoami
+date
+uname -a
+
+
+echo "\n***********************"
